5546dc273d8a655d5c8bece05eeb9d6242256021
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-09-06 22:15:50 -0400

    add: hash to compiled binaries

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -16,8 +16,14 @@ mkdir -p dist || exit 1
 
 for target in "${targets[@]}"; do
   echo "Building $target..."
+  dist_file="dist/lasso-$target"
   bun build --compile --minify --sourcemap --target="bun-$target" \
-    ./src/index.ts --outfile "dist/lasso-$target" || exit 1
+    ./src/index.ts --outfile "$dist_file" || exit 1
+  if [[ -f "$dist_file.exe" ]]; then
+    dist_file="$dist_file.exe"
+  fi
+  hashed="$(openssl dgst -sha256 "$dist_file" | cut -d' ' -f2)" || exit 1
+  echo "$hashed" >"$dist_file.checksum"
 done
 
 echo "Built all targets into dist/"
